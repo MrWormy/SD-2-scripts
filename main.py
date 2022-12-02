19647c43d2adea720ee3d23bd1860e928ed3956d
Thomas <thomas.laurenc@gmail.com>
2022-12-02 11:05:24 +0100

    get prompt from cli

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import sys
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 
 repo_id = "stabilityai/stable-diffusion-2"
@@ -8,7 +9,7 @@ pipe = DiffusionPipeline.from_pretrained(repo_id)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 generator = torch.Generator().manual_seed(52)
 
-negative_prompt = "brown"
-prompt = "Abstract representation of an enlightened priest bear"
+prompt = sys.argv[1] or "An improvised prompt"
+negative_prompt = sys.argv[2] or ""
 image = pipe(prompt, guidance_scale=9, num_inference_steps=20, generator=generator, negative_prompt=negative_prompt).images[0]
 image.save(f"out{round(time.time())}.png")
