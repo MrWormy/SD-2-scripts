2a241121037c29b86cb895c3d9ff1a531ccde3c4
Thomas <thomas.laurenc@gmail.com>
2022-12-02 12:28:09 +0100

    adding more cli options

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 import torch
 import sys
@@ -7,9 +8,11 @@ repo_id = "stabilityai/stable-diffusion-2"
 # torch_dtype=torch.float16, revision="fp16"
 pipe = DiffusionPipeline.from_pretrained(repo_id)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-generator = torch.Generator().manual_seed(52)
+generator = torch.Generator().manual_seed(random.getrandbits(16))
 
 prompt = sys.argv[1] or "An improvised prompt"
 negative_prompt = sys.argv[2] or ""
-image = pipe(prompt, guidance_scale=9, num_inference_steps=20, generator=generator, negative_prompt=negative_prompt).images[0]
+steps = int(sys.argv[3]) or 20
+scale = int(sys.argv[4]) or 9
+image = pipe(prompt, guidance_scale=scale, num_inference_steps=steps, generator=generator, negative_prompt=negative_prompt).images[0]
 image.save(f"out{round(time.time())}.png")
