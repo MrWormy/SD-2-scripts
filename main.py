48e5dc2d2a7626a8b7f34bc6b2dfc6761a15e146
Thomas <thomas.laurenc@gmail.com>
2022-12-03 12:21:48 +0100

    config several files

--- main.py
+++ main.py
@@ -16,5 +16,9 @@ prompt = sys.argv[1] or "An improvised prompt"
 negative_prompt = sys.argv[2] or ""
 steps = int(sys.argv[3]) or 20
 scale = int(sys.argv[4]) or 9
-image = pipe(prompt, guidance_scale=scale, num_inference_steps=steps, generator=generator, negative_prompt=negative_prompt).images[0]
-image.save(f"out{round(time.time())}.png")
+# 3 is max for 20G vram
+images = pipe(prompt, guidance_scale=scale, num_inference_steps=steps, generator=generator,
+              negative_prompt=negative_prompt, num_images_per_prompt=3).images
+
+for image in images:
+    image.save(f"out/out{round(time.time())}_{random.getrandbits(8)}.png")
